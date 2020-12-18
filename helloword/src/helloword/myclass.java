de11db306a9f91a925208d5a9f5266e4f734e280
sai <sai@LAPTOP-EQG3AQIL>
2020-12-18 16:08:32 +0530

    second commit

--- helloword/src/helloword/myclass.java
+++ helloword/src/helloword/myclass.java
@@ -3,9 +3,14 @@ package helloword;
 public class myclass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("test");
+		String s = myfun();
+		System.out.println("Bhushan " + s );
 
 	}
 
+	public static String myfun()
+	{
+		return " Mogal";
+	}
+	
 }
